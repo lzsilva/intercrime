f14d8fc974ac7d3b0d7b3e431cc55fa8f9e9d2b4
Luiz <luiz.slz@hotmail.com>
2018-12-13 00:43:24 -0300

    deleta arquivo app_2.js

--- static/recursos/maps/app.js
+++ static/recursos/maps/app.js
@@ -10,8 +10,34 @@ var map = L.map('map', {
 
 var legend = L.control({position: 'topright'});
 legend.onAdd = function (map) {
-    var div = L.DomUtil.create('div', 'info legend crimes');
-    div.innerHTML = '<select><option></option><option>HOMICIDE</option><option>THEFT</option><option>MOTOR VEHICLE THEFT</option><option>CRIMINAL TRESPASS</option></select>';
+    var div = L.DomUtil.create('div', 'info legend form-group crimes');
+    div.innerHTML = '<label for="select1">Crimes</label>\n' +
+        '<select class="form-control" id="select1"><option></option><option>HOMICIDE</option><option>THEFT</option><option>MOTOR VEHICLE THEFT</option><option>CRIMINAL TRESPASS</option></select>';
+    div.firstChild.onmousedown = div.firstChild.ondblclick = L.DomEvent.stopPropagation;
+    return div;
+};
+legend.addTo(map);
+
+
+var legend = L.control({position: 'topleft'});
+legend.onAdd = function (map) {
+    var div = L.DomUtil.create('div', 'info legend form-group');
+    div.innerHTML = '<label for="date-input1" class="col-form-label">Data inicial</label>\n' +
+        '  <div>\n' +
+        '    <input class="form-control" type="date" id="date-input1">\n' +
+        '  </div>';
+    div.firstChild.onmousedown = div.firstChild.ondblclick = L.DomEvent.stopPropagation;
+    return div;
+};
+legend.addTo(map);
+
+var legend = L.control({position: 'topleft'});
+legend.onAdd = function (map) {
+    var div = L.DomUtil.create('div', 'info legend form-group');
+    div.innerHTML = '<label for="date-input2" class="col-form-label">Data final</label>\n' +
+        '  <div>\n' +
+        '    <input class="form-control" type="date" id="date-input2">\n' +
+        '  </div>';
     div.firstChild.onmousedown = div.firstChild.ondblclick = L.DomEvent.stopPropagation;
     return div;
 };
@@ -24,19 +50,44 @@ $('select').on('change', function () {
     initMap(crimeClicado);
 });
 
-anoClicado = undefined;
+// anoClicado = undefined;
+//
+// $('#ano1, #ano2, #ano3').click(function () {
+//   if (this.id == 'ano1') {
+//     anoClicado = '2018';
+//   }
+//   else if (this.id == 'ano2') {
+//     anoClicado = '2017';
+//   }
+//   else if (this.id == 'ano3') {
+//     anoClicado = '2016';
+//   }
+//   initMap(crimeClicado, anoClicado);
+// });
 
-$('#ano1, #ano2, #ano3').click(function () {
-    if (this.id == 'ano1') {
-        anoClicado = '2018';
+dateInicial = undefined;
+dateFinal = undefined;
+
+$('#date-input1').change(function () {
+    if ($('#date-input1').val() !== '') {
+        dateInicial = $('#date-input1').val() + 'T00:01:00.000Z';
+
+        if($('#date-input2').val() === '')
+            initMap(crimeClicado, dateInicial, dateInicial);
+        else
+            initMap(crimeClicado, dateInicial, dateFinal);
     }
-    else if (this.id == 'ano2') {
-        anoClicado = '2017';
+});
+
+$('#date-input2').change(function () {
+    if ($('#date-input2').val() !== '') {
+        dateFinal = $('#date-input2').val() + 'T00:01:00.000Z';
+
+        if($('#date-input1').val() === '')
+            initMap(crimeClicado, dateFinal, dateFinal);
+        else
+            initMap(crimeClicado, dateInicial, dateFinal);
     }
-    else if (this.id == 'ano3') {
-        anoClicado = '2016';
-    }
-    initMap(crimeClicado, anoClicado);
 });
 
 L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -56,62 +107,75 @@ var myIcon = L.icon({
 
 var markerClusters = L.markerClusterGroup();
 
-initMap(crimeClicado, anoClicado);
+initMap(crimeClicado, dateFinal, dateFinal);
 
-function initMap(crimeClicado, anoClicado) {
+function initMap(crimeClicado, dateInicial, dateFinal) {
 
     markerClusters.clearLayers();
 
-	url = 'http://cidadesinteligentes.lsdi.ufma.br/collector/resources/data/';
+    url = 'http://cidadesinteligentes.lsdi.ufma.br/collector/resources/data/';
 
-	filtros = {"capabilities": ["incidencia_crime"], "matchers":{} }
-	
-	
-    if (!(crimeClicado == '') && !(crimeClicado == 'Crime'))	
-		filtros["matchers"]["crime_type.eq"] = crimeClicado;
- 
+    filtros = {"capabilities": ["incidencia_crime"], "matchers": {}};
 
-    if (anoClicado === undefined)
-            filtros["matchers"]["year.in"] = [2016, 2017,2018];
-	else		
-		filtros["matchers"]["year.eq"] = anoClicado;
-	
 
-		
-    $.post(url, filtros,  function (data, textStatus) {
+    if (!(crimeClicado == '') && !(crimeClicado == 'Crime'))
+        filtros["matchers"]["crime_type.eq"] = crimeClicado;
+
+
+    // if (anoClicado === undefined)
+    //   filtros["matchers"]["year.in"] = [2016, 2017, 2018];
+    // else
+    //   filtros["matchers"]["year.eq"] = anoClicado;
+
+    // if (dateInicial === '' && dateFinal === '')
+    //   filtros["matchers"]["year.in"] = [2016, 2017, 2018];
+
+    if (dateInicial !== undefined || dateInicial !== '') {
+        filtros["start_date"] = dateInicial;
+    } else {
+        filtros["start_date"] = "2016-01-01T00:01:00.000Z";
+    }
+
+    if (dateFinal !== undefined || dateFinal !== '') {
+        filtros["end_date"] = dateFinal;
+    } else {
+        filtros["end_date"] = "2018-12-12T00:01:00.000Z";
+    }
+
+    $.post(url, filtros, function (data, textStatus) {
 
         $.each(data.resources, function (i, recurso) {
-			$.each(recurso.capabilities.incidencia_crime, function (j, crime) {
+            $.each(recurso.capabilities.incidencia_crime, function (j, crime) {
 
-				var houve_prisao = crime.arrest ? 'Sim' : 'Não';
+                var houve_prisao = crime.arrest ? 'Sim' : 'Não';
 
-		        var popup = '<div id="content">' +
-		            '<div id="siteNotice">' +
-		            '</div>' +
-		            '<h1 id="firstHeading" class="firstHeading">' + crime.location_description + '</h1>' +
-		            '<div id="bodyContent">' +
-		            '<p><b>Crime primário:</b> ' + crime.crime_type + '</p>' +
-		            '<p><b>Descrição do crime:</b> ' + crime.description + '</p>' +
-		            '<p><b>Bloco:</b> ' + crime.block + '</p>' +
-		            '<p><b>Data do evento:</b> ' + crime.date + '</p>' +
-		            '<p><b>Houve prisão:</b> ' + houve_prisao + '</p>' +
-		            '<p><b>Ano:</b> ' + crime.year + '</p>' +
+                var popup = '<div id="content">' +
+                    '<div id="siteNotice">' +
+                    '</div>' +
+                    '<h1 id="firstHeading" class="firstHeading">' + crime.location_description + '</h1>' +
+                    '<div id="bodyContent">' +
+                    '<p><b>Crime primário:</b> ' + crime.crime_type + '</p>' +
+                    '<p><b>Descrição do crime:</b> ' + crime.description + '</p>' +
+                    '<p><b>Bloco:</b> ' + crime.block + '</p>' +
+                    '<p><b>Data do evento:</b> ' + crime.date + '</p>' +
+                    '<p><b>Houve prisão:</b> ' + houve_prisao + '</p>' +
+                    '<p><b>Ano:</b> ' + crime.year + '</p>' +
 
-		            '<p>Coordenadas:</p>' +
-		            '<p><b>Latitude:</b> ' + crime.lat + '</p>' +
-		            '<p><b>Longitude:</b> ' + crime.lon + '</p>' +
-		            '</div>' +
-		            '</div>';
+                    '<p>Coordenadas:</p>' +
+                    '<p><b>Latitude:</b> ' + crime.lat + '</p>' +
+                    '<p><b>Longitude:</b> ' + crime.lon + '</p>' +
+                    '</div>' +
+                    '</div>';
 
-				if (crime.lat && crime.lon){
+                if (crime.lat && crime.lon) {
 
-				    var m = L.marker([crime.lat, crime.lon], {icon: myIcon})
-				        .bindPopup(popup);
+                    var m = L.marker([crime.lat, crime.lon], {icon: myIcon})
+                        .bindPopup(popup);
 
-				    markerClusters.addLayer(m);
-				}
-				
-			});
+                    markerClusters.addLayer(m);
+                }
+
+            });
 
         });
     }, "json");
@@ -119,8 +183,3 @@ function initMap(crimeClicado, anoClicado) {
     map.addLayer(markerClusters);
 
 }
-
-
-$( "#datepicker" ).datepicker();
-
-
